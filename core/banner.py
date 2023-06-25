f180aa15368607731d50c83179a9391edbbaab65
Eyup Sukru ERGIN <ergindev@gmail.com>
2023-06-25 21:42:52 +0300

    Create banner.py

---
+++ core/banner.py
@@ -0,0 +1,10 @@
+def banner():
+    print('''
+______  __             ________________________
+___  / / ____  __________  /___  __ __  /___  /______  _________  |  Version: v1.3
+__  /_/ /_  / / __  __ _  ____  /_/ _  ____  __ __  / / __  ___/  |  Developed by Eyup Sukru ERGIN
+_  __  / / /_/ /_  / / / /_ _  _, _// /_ _  / / _  /_/ /_(__  )   |  --------------------------------------
+/_/ /_/  \__,_/ /_/ /_/\__/ /_/ |_| \__/ /_/ /_/_\__, / /____/    |  https://ergin.dev
+                                                /____/            |  https://github.com/eyupergin/huntrthys
+
+Specific Rhadamanthys Command and Control Server Detection Tool'''+ "\n")
